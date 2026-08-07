a33f2ad0ebbaa64c4401b9530241bba61b867edf
KaiTutor <miltonkaitutor@gmail.com>
2026-08-06 21:27:06 -0400

    updated reviews

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -4,18 +4,18 @@
 
 export const REVIEWS = [
   {
-    title: 'Great Resource!',
-    rating: 5,
-    author: 'FQTN',
-    date: 'May 2',
-    text: 'Very easy to use and has tons of useful tools to learn and grow in faith',
-  },
-  {
     title: 'Amazing App',
     rating: 5,
     author: 'Edsel7334',
     date: 'Jun 13',
     text: "I have tried many Orthodox calendar apps, Bible apps, study apps, etc., and while I continue to use a few others for various reasons (eg. daily sayings from Church Fathers or Saints), this one is by far the best and my favorite. It combines the best aspects of all the others, is 100% free and hides nothing behind a paywall (although I donate monthly to support development — that's how wonderful I think it is), is elegant and polished, easy to use, and is most conducive and helpful to my study of the Faith. It includes beautiful prayers for every occasion, is easy to navigate, and has an excellent easy-to-use calendar as well (trust me, I've tried many). Wonderful job by the developers and may God bless you and strengthen you to keep up the good work!",
+  },
+  {
+    title: 'Great Resource!',
+    rating: 5,
+    author: 'FQTN',
+    date: 'May 2',
+    text: 'Very easy to use and has tons of useful tools to learn and grow in faith',
   },
   {
     title: 'Best Free Orthodox App!',
